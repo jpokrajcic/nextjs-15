3ee45fcc6cefbdf34c8449d87c72f403bc195dfd
Josip Pokrajcic <wastelander2929@gmail.com>
2024-11-27 11:22:44 +0100

    Tailwind config and fonts setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,11 @@
 export default function Home() {
   console.log("Some server magic!");
-  return <div className="text-2xl">START WORKING</div>;
+  return (
+    <>
+      <h1 className="text-2xl">Inter font raznosi</h1>
+      <h1 className="text-2xl">Space grotesque do kkraja</h1>
+
+      <h1 className="testKlasa1 text-2xl">Test jedan grotesq</h1>
+    </>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,39 @@
-import type { Config } from 'tailwindcss';
+import type {Config} from "tailwindcss";
 
 export default {
+  darkMode: "selector",
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        background: 'var(--background)',
-        foreground: 'var(--foreground)',
+        primary: {
+          100: "hsla(26, 100%, 50%, 1)",
+          500: "hsla(26, 100%, 95%, 1)",
+        },
+        dark: {
+          100: "hsla(0, 0%, 0%, 1)",
+          200: "hsla(225, 21%, 7%, 1)",
+          300: "hsla(223, 22%, 11%, 1)",
+          400: "hsla(223, 22%, 17%, 1)",
+          500: "hsla(229, 14%, 29%, 1)",
+        },
+        light: {
+          900: "hsla(0, 0%, 100%, 1)",
+          850: "hsla(0, 0%, 99%, 1)",
+          800: "hsla(210, 22%, 96%, 1)",
+          700: "hsla(219, 43%, 90%, 1)",
+          500: "hsla(225, 41%, 63%, 1)",
+          400: "hsla(227, 20%, 60%, 1)",
+        },
+        foreground: "var(--foreground)",
+      },
+      fontFamily: {
+        inter: ["var(--font-inter)"],
+        spacegrotesk: ["var(--font-space-grotesk)"],
       },
     },
   },
