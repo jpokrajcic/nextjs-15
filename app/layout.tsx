176be3b791039b209db84ddb5af13b1257ef20bb
Josip Pokrajcic <wastelander2929@gmail.com>
2024-11-28 12:48:44 +0100

    Add and setup next-themes package and install shadCN

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type {Metadata} from "next";
 import {Inter, Space_Grotesk as SpaceGrotesk} from "next/font/google";
+
 import "./globals.css";
+import ThemeProvider from "@/context/ThemeProvider";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -29,11 +31,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${inter.variable} ${spaceGrotesk.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          disableTransitionOnChange
+          enableSystem
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import type {Config} from "tailwindcss";
 
 export default {
-  darkMode: "selector",
+  darkMode: ["selector", "class"],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -11,23 +11,24 @@ export default {
     extend: {
       colors: {
         primary: {
-          100: "hsla(26, 100%, 50%, 1)",
-          500: "hsla(26, 100%, 95%, 1)",
+          "100": "hsla(26, 100%, 50%, 1)",
+          "500": "hsla(26, 100%, 95%, 1)",
         },
+
         dark: {
-          100: "hsla(0, 0%, 0%, 1)",
-          200: "hsla(225, 21%, 7%, 1)",
-          300: "hsla(223, 22%, 11%, 1)",
-          400: "hsla(223, 22%, 17%, 1)",
-          500: "hsla(229, 14%, 29%, 1)",
+          "100": "hsla(0, 0%, 0%, 1)",
+          "200": "hsla(225, 21%, 7%, 1)",
+          "300": "hsla(223, 22%, 11%, 1)",
+          "400": "hsla(223, 22%, 17%, 1)",
+          "500": "hsla(229, 14%, 29%, 1)",
         },
         light: {
-          900: "hsla(0, 0%, 100%, 1)",
-          850: "hsla(0, 0%, 99%, 1)",
-          800: "hsla(210, 22%, 96%, 1)",
-          700: "hsla(219, 43%, 90%, 1)",
-          500: "hsla(225, 41%, 63%, 1)",
-          400: "hsla(227, 20%, 60%, 1)",
+          "400": "hsla(227, 20%, 60%, 1)",
+          "500": "hsla(225, 41%, 63%, 1)",
+          "700": "hsla(219, 43%, 90%, 1)",
+          "800": "hsla(210, 22%, 96%, 1)",
+          "850": "hsla(0, 0%, 99%, 1)",
+          "900": "hsla(0, 0%, 100%, 1)",
         },
         foreground: "var(--foreground)",
       },
@@ -35,7 +36,12 @@ export default {
         inter: ["var(--font-inter)"],
         spacegrotesk: ["var(--font-space-grotesk)"],
       },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
