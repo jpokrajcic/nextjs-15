3ea8da8b1fe841b262d153c6cb56b90e0a6769ea
Josip Pokrajcic <wastelander2929@gmail.com>
2024-11-30 14:22:09 +0100

    Enable OAuth with Google

--- auth.ts
+++ auth.ts
@@ -1,7 +1,8 @@
 // This will initialize next-auth and middleware uses 'auth'
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 
 export const {handlers, signIn, signOut, auth} = NextAuth({
-  providers: [GitHub],
+  providers: [GitHub, Google],
 });
